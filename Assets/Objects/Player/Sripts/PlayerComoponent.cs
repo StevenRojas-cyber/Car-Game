7fe5357916461c90ae1e5de746f31d98ae77a3c6
StevenRojas-cyber <stevenisaac3103@gmail.com>
2026-07-22 21:38:09 -0300

    PlayerMovement

--- Assets/Objects/Player/Sripts/PlayerComoponent.cs
+++ Assets/Objects/Player/Sripts/PlayerComoponent.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerComoponent : MonoBehaviour
 {
 
 
+    [Header("Player Atributes")]
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private float moveSpeed;
+
+    private float moveDirection;
+
     void Start()
     {
-              
+           moveAction.action.Enable();
     }
 
     void Update()
     {
-        
+        if (moveAction == null) return;
+
+
+        moveDirection = moveAction.action.ReadValue<float>();
+
+        print(moveDirection);
+
+        Vector3 movement = new Vector3(moveDirection * moveSpeed * Time.deltaTime, 0, 0);
+
+        transform.Translate(movement);
     }
 }
