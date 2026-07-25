6e3a22c9460faf16d409736061d47d8d170ed45c
StevenRojas-cyber <stevenisaac3103@gmail.com>
2026-07-25 16:56:37 -0300

    Implementación del motor 1

--- Assets/Objects/Motors/1/Script/FuelPipe.cs
+++ Assets/Objects/Motors/1/Script/FuelPipe.cs
@@ -7,10 +7,18 @@ using UnityEngine.UIElements;
 public class FuelPipe : MotorPart, IPointerDownHandler, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     [Header("Fuel Pipe Settings")]
-    [SerializeField] private float MaxFuelKeyLaps = 5;
+    [SerializeField] private int MaxFuelKeyLaps = 5;
+    [SerializeField] private float ReverseLapsMultiplier = 1f;
+    [SerializeField] private float UnWindSpeed = 180f;
+    [SerializeField] private SpriteRenderer Sprite;
 
+
+    private bool AreLapsCompleted = false;
+    private int MaxLapsInDegrees;
+    private int currentNumberOfLaps;
     private float PreviousAngle = 0f;
     private float AngleAcumulated;
+
 
 
     private Camera mainCamera;
@@ -20,20 +28,64 @@ public class FuelPipe : MotorPart, IPointerDownHandler, IDragHandler, IBeginDrag
         PartName = "Fuel Pipe";
         currentState = PartsStates.Repaired;
 
+
+        MaxLapsInDegrees = MaxFuelKeyLaps * 360;
         mainCamera = Camera.main;
+        transform.rotation = Quaternion.Euler(0,0,MaxLapsInDegrees);
     }
 
     
     void Update()
     {
-        float MaxLapsInRadians = MaxFuelKeyLaps * Mathf.Deg2Rad;
-        //Debug.Log($"Max Laps in Radians: {MaxLapsInRadians}");
+        DiscountLaps();
+
+        if(AreLapsCompleted)
+        {
+            currentState = PartsStates.Repaired;
+            Sprite.color = Color.green;
+        }
+        else
+        {
+            currentState = PartsStates.Damaged;
+            AreLapsCompleted = false;
+            Sprite.color = Color.red;
+        }
     }
     
     
-    
+    void DiscountLaps()
+    {
+        if (AngleAcumulated < 0)
+        {
+            // Sumamos grados para acercarlo de vuelta a 0 (desenroscar)
+            AngleAcumulated += UnWindSpeed * ReverseLapsMultiplier * Time.deltaTime;
+
+            // Limitamos para que no se pase de 0 (posición de reposo)
+            if (AngleAcumulated > 0)
+            {
+                AngleAcumulated = 0f;
+            }
+
+            // Aplicamos la rotación visual
+            transform.rotation = Quaternion.Euler(0, 0, AngleAcumulated);
+
+            // Recalculamos las vueltas actuales usando el valor absoluto
+            currentNumberOfLaps = (int)(Mathf.Abs(AngleAcumulated) / 360f);
+
+            // Si las vueltas caen por debajo del máximo, ya no están completas
+            if (currentNumberOfLaps < MaxFuelKeyLaps)
+            {
+                AreLapsCompleted = false;
+            }
+        }
+    }
+
+
+    //Logica de rotaion con el mouse
     public void OnBeginDrag(PointerEventData eventData)
     {
+        if (AreLapsCompleted) return;
+
         Vector3 posicionRaton = mainCamera.ScreenToWorldPoint(eventData.position);
         posicionRaton.z = 0;
         Vector3 direccion = posicionRaton - transform.position;
@@ -43,29 +95,41 @@ public class FuelPipe : MotorPart, IPointerDownHandler, IDragHandler, IBeginDrag
 
     public void OnDrag(PointerEventData eventData)
     {
+        if (AreLapsCompleted) return;
+
         Vector3 posicionRaton = mainCamera.ScreenToWorldPoint(eventData.position);
         posicionRaton.z = 0;
-
         Vector3 direccion = posicionRaton - transform.position;
 
         float anguloActual = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
-
         
         float diferencia = Mathf.DeltaAngle(PreviousAngle, anguloActual);
-
         
-        AngleAcumulated += diferencia;
 
-        
-        PreviousAngle = anguloActual;
-
-
-        if(AngleAcumulated < PreviousAngle && AngleAcumulated < 0)
-        { 
-            transform.rotation = Quaternion.Euler(0, 0, AngleAcumulated);
-            Debug.Log("Current Angle (Degrees): " + AngleAcumulated);
+        if(diferencia < 0)
+        {
+            AngleAcumulated += diferencia;
+            PreviousAngle = anguloActual;
         }
+
         
+        transform.rotation = Quaternion.Euler(0, 0, AngleAcumulated);
+      
+
+        currentNumberOfLaps = ( (int)(AngleAcumulated / 360f) ) * -1;
+
+
+        if( currentNumberOfLaps >= MaxFuelKeyLaps )
+        {
+            AreLapsCompleted = true;
+            Debug.Log("Listas Las vueltas!!");
+        }
+        else
+        {
+            Debug.Log("Laps: " +  currentNumberOfLaps);
+        }
+
+
     }
 
     public void OnEndDrag(PointerEventData eventData)
